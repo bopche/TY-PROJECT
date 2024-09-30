0e7979b8deb6df14fd93e5e22150bfa666dc2525
nidhijangde09 <jangdenidhi74@gmail.com>
2024-09-30 20:47:47 +0530

    slider added

---
+++ JS/index.js
@@ -0,0 +1,43 @@
+let sidemeu = document.getElementById("sideMenu");
+function openMenu() {
+    sidemeu.style.top = "0";
+}
+function closeMenu() {
+    sidemeu.style.top = "-400px";
+}
+
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("demo");
+    let captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
+
+
