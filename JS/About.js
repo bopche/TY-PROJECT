74d19d56200083d040a46099cb9d8f1553946071
shreyaa-kar <shreyakar444@gmail.com>
2025-01-02 15:25:42 +0530

    commit

--- JS/About.js
+++ JS/About.js
@@ -1,2 +1,11 @@
 // Placeholder for future interactivity
 console.log("Welcome to Odyssey's About Us page!");
+// script.js
+const carouselTrack = document.querySelector('.carousel-track');
+
+// Duplicate the cards dynamically for infinite scrolling
+const teamCards = Array.from(carouselTrack.children);
+teamCards.forEach(card => {
+  const clone = card.cloneNode(true);
+  carouselTrack.appendChild(clone);
+});
